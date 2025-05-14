1db2d565eebda2c5cf60f75bb7a8269008858609
Jan Sala <jasiek.sala98@gmail.com>
2025-05-14 23:20:32 +0200

    chore: copy tymek's notebook and setup.py

---
+++ JS/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mastercard_sgh_neural_noobs',
+    version='0.1',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+)
